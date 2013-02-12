3c2f74f52a9dd7cf771c394f9c0325bd496f1f8a
wrongu <richard.d.lange.13@dartmouth.edu>
2013-02-12 10:21:09 -0500

    ignoring temp build files

--- Debug/functors.d
+++
@@ -1,3 +0,0 @@
-functors.d functors.o: ../functors.cpp ../functors.hpp
-
-../functors.hpp:
--- Debug/math_helpers.d
+++
@@ -1,8 +0,0 @@
-math_helpers.d math_helpers.o: ../math_helpers.cpp ../math_helpers.hpp \
-  /usr/local/include/opencv/cv.h ../functors.hpp
-
-../math_helpers.hpp:
-
-/usr/local/include/opencv/cv.h:
-
-../functors.hpp:
