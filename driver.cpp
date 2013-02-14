793346f9ff071631f115f7999a87116900794394
wrongu <richard.d.lange.13@dartmouth.edu>
2013-02-14 10:31:01 -0500

    got driver options parsing properly

--- driver.cpp
+++ driver.cpp
@@ -18,17 +18,28 @@ struct options {
 	std::string file_out;
 };
 
-PARSE_CONTEXT parse_opt(const char* str, options & opts, PARSE_CONTEXT ctx = NONE){
+PARSE_CONTEXT parse_opt(char* str, options & opts, PARSE_CONTEXT ctx = NONE){
 	switch(ctx){
 	case OUTPUT:
 		opts.file_out = str;
+		std::cout << "set output file to " << opts.file_out << std::endl;
 		return NONE;
 	case INPUT:
 		opts.file_in = str;
+		std::cout << "set input file to " << opts.file_out << std::endl;
 		return NONE;
 	case ALGORITHM:
-		// set string so that the --f, --s, etc parser does it
-		str = ("--" + str[0]);
+		std::cout << "setting algorithm option" << std::endl;
+		switch(str[0]){
+			case 'f':
+				std::cout << "set algo to features" << std::endl;
+				opts.algorithm = FEATURES;
+				break;
+			case 's':
+				std::cout << "set algo to segmentation" << std::endl;
+				opts.algorithm = SEGMENTATION;
+				break;
+		}
 		break;
 	case NONE:
 	default:
@@ -52,23 +63,31 @@ PARSE_CONTEXT parse_opt(const char* str, options & opts, PARSE_CONTEXT ctx = NON
 		//	--s specify segmentation test
 		switch(str[1]){
 		case 'o':
+			std::cout << "set ctx to output" << std::endl;
 			return OUTPUT;
 		case 'f':
+			std::cout << "set ctx to input" << std::endl;
 			return INPUT;
 		case 'a':
+			std::cout << "set ctx to algorithm" << std::endl;
 			return ALGORITHM;
 		case 'v':
+			std::cout << "set disp to true" << std::endl;
 			opts.display = true;
 			break;
 		case 'h':
+			std::cout << "set disp to false" << std::endl;
 			opts.display = false;
 			break;
 		case '-':
+			std::cout << "double option" << std::endl;
 			switch(str[2]){
 			case 'f':
+				std::cout << "set algo to features" << std::endl;
 				opts.algorithm = FEATURES;
 				break;
 			case 's':
+				std::cout << "set algo to segmentation" << std::endl;
 				opts.algorithm = SEGMENTATION;
 				break;
 			}
@@ -86,8 +105,9 @@ int main(int argc, char** argv){
 	std::string default_file_out = "output/result.jpg";
 	// set up 'options' struct, parse it
 	options o = {true, FEATURES, "", ""};
-	for(int i=2; i < argc; ++i)
-		parse_opt(argv[i], o);
+	PARSE_CONTEXT next_ctx = NONE;
+	for(int i=1; i < argc; ++i)
+		next_ctx = parse_opt(argv[i], o, next_ctx);
 
 	if(o.file_in == "")  o.file_in = default_file_in;
 	if(o.file_out == "") o.file_out = default_file_out;
