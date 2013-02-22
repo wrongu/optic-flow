a3c6ca1299296fd8348dc7b62cbdc22c7d70285a
wrongu <richard.d.lange.13@dartmouth.edu>
2013-02-22 11:16:54 -0500

    added psi_deriv functor

--- functors.cpp
+++ functors.cpp
@@ -20,3 +20,7 @@ double gauss1_exp(double x, double sigma2){
 double gauss2_exp(double x, double sigma2){
 	return exp(-(x*x) / (2 * sigma2)) * (x*x/sigma2 - 1);
 }
+
+double psi_deriv(double s_2, double eps_2){
+	return 1.0 / (2.0 * sqrt(s_2 + eps_2));
+}
